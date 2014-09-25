3038d148ce8481b001d07abcd863f513805452f0
Edi Modrić <edi.modric@gmail.com>
2014-09-25 16:43:38 +0200

    CS fixes

--- Manipulator/KernelManipulator.php
+++ Manipulator/KernelManipulator.php
@@ -3,6 +3,8 @@
 namespace Netgen\Bundle\GeneratorBundle\Manipulator;
 
 use Symfony\Component\HttpKernel\KernelInterface;
+use ReflectionObject;
+use RuntimeException;
 
 class KernelManipulator extends Manipulator
 {
@@ -14,10 +16,10 @@ class KernelManipulator extends Manipulator
      *
      * @param KernelInterface $kernel A KernelInterface instance
      */
-    public function __construct(KernelInterface $kernel)
+    public function __construct( KernelInterface $kernel )
     {
         $this->kernel = $kernel;
-        $this->reflected = new \ReflectionObject($kernel);
+        $this->reflected = new ReflectionObject( $kernel );
     }
 
     /**
@@ -29,43 +31,49 @@ class KernelManipulator extends Manipulator
      *
      * @throws \RuntimeException If bundle is already defined
      */
-    public function addBundle($bundle)
+    public function addBundle( $bundle )
     {
-        if (!$this->reflected->getFilename()) {
+        if ( !$this->reflected->getFilename() )
+        {
             return false;
         }
 
-        $src = file($this->reflected->getFilename());
-        $method = $this->reflected->getMethod('registerBundles');
-        $lines = array_slice($src, $method->getStartLine() - 1, $method->getEndLine() - $method->getStartLine() + 1);
+        $src = file( $this->reflected->getFilename() );
+        $method = $this->reflected->getMethod( 'registerBundles' );
+        $lines = array_slice( $src, $method->getStartLine() - 1, $method->getEndLine() - $method->getStartLine() + 1 );
 
         // Don't add same bundle twice
-        if (false !== strpos(implode('', $lines), $bundle)) {
-            throw new \RuntimeException(sprintf('Bundle "%s" is already defined in "AppKernel::registerBundles()".', $bundle));
+        if ( strpos( implode( '', $lines ), $bundle ) !== false )
+        {
+            throw new RuntimeException( sprintf( 'Bundle "%s" is already defined in "%s::registerBundles()".', $this->reflected->getName(), $bundle ) );
         }
 
-        $this->setCode(token_get_all('<?php '.implode('', $lines)), $method->getStartLine());
-        while ($token = $this->next()) {
-            if ($token[0] !== T_RETURN) {
+        $this->setCode( token_get_all( '<?php ' . implode( '', $lines ) ), $method->getStartLine() );
+        while ( $token = $this->next() )
+        {
+            if ( $token[0] !== T_RETURN )
+            {
                 continue;
             }
 
-            if (trim(implode('',array_slice($src, $this->line - 2, 1))) == "") {
+            if ( trim( implode( '', array_slice( $src, $this->line - 2, 1 ) ) ) == "" )
+            {
                 $lines = array_merge(
-                    array_slice($src, 0, $this->line - 2),
-                    array(sprintf("        \$bundles[] = new \\%s();\n", $bundle)),
-                    array_slice($src, $this->line - 2)
+                    array_slice( $src, 0, $this->line - 2 ),
+                    array( sprintf( "        \$bundles[] = new \\%s();\n", $bundle ) ),
+                    array_slice( $src, $this->line - 2 )
                 );
             }
-            else {
+            else
+            {
                 $lines = array_merge(
-                    array_slice($src, 0, $this->line - 1),
-                    array(sprintf("        \$bundles[] = new \\%s();\n", $bundle)),
-                    array_slice($src, $this->line - 1)
+                    array_slice( $src, 0, $this->line - 1 ),
+                    array( sprintf( "        \$bundles[] = new \\%s();\n", $bundle ) ),
+                    array_slice( $src, $this->line - 1 )
                 );
             }
 
-            file_put_contents($this->reflected->getFilename(), implode('', $lines));
+            file_put_contents( $this->reflected->getFilename(), implode( '', $lines ) );
 
             return true;
         }
--- Manipulator/RoutingManipulator.php
+++ Manipulator/RoutingManipulator.php
@@ -3,6 +3,7 @@
 namespace Netgen\Bundle\GeneratorBundle\Manipulator;
 
 use Symfony\Component\DependencyInjection\Container;
+use RuntimeException;
 
 class RoutingManipulator extends Manipulator
 {
@@ -13,7 +14,7 @@ class RoutingManipulator extends Manipulator
      *
      * @param string $file The YAML routing file path
      */
-    public function __construct($file)
+    public function __construct( $file )
     {
         $this->file = $file;
     }
@@ -30,29 +31,37 @@ class RoutingManipulator extends Manipulator
      *
      * @throws \RuntimeException If bundle is already imported
      */
-    public function addResource($bundle, $format, $prefix = '/', $path = 'routing')
+    public function addResource( $bundle, $format, $prefix = '/', $path = 'routing' )
     {
         $current = '';
-        if (file_exists($this->file)) {
-            $current = file_get_contents($this->file);
+        if ( file_exists( $this->file ) )
+        {
+            $current = file_get_contents( $this->file );
 
             // Don't add same bundle twice
-            if (false !== strpos($current, $bundle)) {
-                throw new \RuntimeException(sprintf('Bundle "%s" is already imported.', $bundle));
+            if ( strpos( $current, $bundle ) !== false )
+            {
+                throw new RuntimeException( sprintf( 'Bundle "%s" is already imported.', $bundle ) );
             }
-        } elseif (!is_dir($dir = dirname($this->file))) {
-            mkdir($dir, 0777, true);
+        }
+        else if ( !is_dir( $dir = dirname( $this->file ) ) )
+        {
+            mkdir( $dir, 0777, true );
         }
 
-        $code = "\n" . sprintf("%s:\n", Container::underscore(substr($bundle, 0, -6)).('/' !== $prefix ? '_'.str_replace('/', '_', substr($prefix, 1)) : ''));
-        if ('annotation' == $format) {
-            $code .= sprintf("    resource: \"@%s/Controller/\"\n    type: annotation\n", $bundle);
-        } else {
-            $code .= sprintf("    resource: \"@%s/Resources/config/%s.%s\"\n", $bundle, $path, $format);
+        $code = "\n" . sprintf( "%s:\n", Container::underscore( substr( $bundle, 0, -6 ) ) . ( '/' !== $prefix ? '_' . str_replace( '/', '_', substr( $prefix, 1 ) ) : '' ) );
+        if ( $format == 'annotation' )
+        {
+            $code .= sprintf( "    resource: \"@%s/Controller/\"\n    type: annotation\n", $bundle );
+        }
+        else
+        {
+            $code .= sprintf( "    resource: \"@%s/Resources/config/%s.%s\"\n", $bundle, $path, $format );
         }
         $code = $current . $code;
 
-        if (false === file_put_contents($this->file, $code)) {
+        if ( file_put_contents( $this->file, $code ) === false )
+        {
             return false;
         }
 
