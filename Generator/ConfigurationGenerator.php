946b75d30bb991b6539dcf53ddaab42e55cae34f
Edi Modrić <edi.modric@gmail.com>
2015-04-03 13:58:13 +0200

    Adopt generator to changed structure of ezpublish.yml

--- Generator/ConfigurationGenerator.php
+++ Generator/ConfigurationGenerator.php
@@ -25,21 +25,9 @@ class ConfigurationGenerator extends Generator
         $settings['imports'] = array(
             array( 'resource' => '@NetgenMoreBundle/Resources/config/ezpublish.yml' ),
             array( 'resource' => '@NetgenMoreLegacyBundle/Resources/config/ezpublish.yml' ),
-            array( 'resource' => '@' . $input->getOption( 'bundle-name' ) . '/Resources/config/ezpublish.yml' )
+            array( 'resource' => '@' . $input->getOption( 'bundle-name' ) . '/Resources/config/ezpublish.yml' ),
+            array( 'resource' => 'ezpublish_system.yml' ),
         );
-
-        // HTTP cache and ImageMagick settings
-
-        $settings['ezpublish']['http_cache']['purge_type'] = '%http_cache.purge_type%';
-        $settings['ezpublish']['imagemagick']['enabled'] = true;
-        $settings['ezpublish']['imagemagick']['path'] = '%imagemagick_path%';
-
-        // Repository definitions
-
-        $projectNormalized = Container::underscore( $input->getOption( 'project' ) );
-        $doctrineRepositoryName = $projectNormalized . '_repository';
-        $settings['ezpublish']['repositories'][$doctrineRepositoryName]['engine'] = 'legacy';
-        $settings['ezpublish']['repositories'][$doctrineRepositoryName]['connection'] = 'default';
 
         // List of siteaccesses and groups
 
@@ -63,14 +51,8 @@ class ConfigurationGenerator extends Generator
         $settings['ezpublish']['siteaccess']['list'] = $siteAccessNames;
         $settings['ezpublish']['siteaccess']['list'][] = $adminSiteAccessName;
 
-        $groupName = $projectNormalized . '_group';
-        $settings['ezpublish']['siteaccess']['groups'][$groupName] = $settings['ezpublish']['siteaccess']['list'];
-
-        $frontendGroupName = $projectNormalized . '_frontend_group';
-        $settings['ezpublish']['siteaccess']['groups'][$frontendGroupName] = $siteAccessNames;
-
-        $administrationGroupName = $projectNormalized . '_administration_group';
-        $settings['ezpublish']['siteaccess']['groups'][$administrationGroupName] = array( $adminSiteAccessName );
+        $settings['ezpublish']['siteaccess']['groups']['frontend_group'] = $siteAccessNames;
+        $settings['ezpublish']['siteaccess']['groups']['administration_group'] = array( $adminSiteAccessName );
 
         // Siteaccess match settings
 
@@ -100,17 +82,11 @@ class ConfigurationGenerator extends Generator
             }
         }
 
-        // List of global settings and siteaccess languages
+        // List of siteaccess languages
 
         $settings['ezpublish']['system'] = array();
 
-        $settings['ezpublish']['system'][$groupName]['repository'] = $doctrineRepositoryName;
-        $settings['ezpublish']['system'][$groupName]['var_dir'] = 'var/ezdemo_site';
-        $settings['ezpublish']['system'][$groupName]['http_cache'] = array(
-            'purge_servers' => '%http_cache.purge_servers%'
-        );
-
-        $settings['ezpublish']['system'][$frontendGroupName]['translation_siteaccesses'] = $siteAccessNames;
+        $settings['ezpublish']['system']['frontend_group']['translation_siteaccesses'] = $siteAccessNames;
 
         foreach ( $siteAccessList as $siteAccessName => $siteAccessLanguages )
         {
@@ -122,15 +98,7 @@ class ConfigurationGenerator extends Generator
 
         $settings['ezpublish']['system'][$adminSiteAccessName]['languages'] = $adminSiteAccessLanguages;
 
-        $settings['ezpublish']['system']['default']['content']['view_cache'] = true;
-        $settings['ezpublish']['system']['default']['content']['ttl_cache'] = true;
-        $settings['ezpublish']['system']['default']['content']['default_ttl'] = 300;
-
         $settings['ez_publish_legacy']['system'][$adminSiteAccessName]['legacy_mode'] = true;
-
-        // Stash settings
-
-        $settings['stash'] = $this->getStashCacheSettings();
 
         file_put_contents(
             $this->container->getParameter( 'kernel.root_dir' ) . '/config/ezpublish.yml',
@@ -151,53 +119,6 @@ class ConfigurationGenerator extends Generator
             array(
                 '',
                 'Generated <comment>ezpublish.yml</comment> configuration file!'
-            )
-        );
-    }
-
-    /**
-     * Returns cache settings based on which cache functionality is available on the current server
-     *
-     * Order of preference:
-     * - FileSystem
-     * - APC
-     * - Memcache  [DISABLED, SEE INLINE]
-     * - Xcache  [DISABLED, SEE INLINE]
-     * - variable instance cache  [DISABLED, SEE INLINE]
-     *
-     * @return array
-     */
-    protected function getStashCacheSettings()
-    {
-        // Should only contain one out of the box
-        $handlers = array();
-        $handlerSetting = array();
-        if ( FileSystem::isAvailable() )
-        {
-            $handlers[] = 'FileSystem';
-            // If running on Windows, use "crc32" keyHashFunction
-            if ( stripos( php_uname(), 'win' ) === 0 )
-            {
-                $handlerSetting['FileSystem'] = array(
-                    'keyHashFunction' => 'crc32'
-                );
-            }
-        }
-        else
-        {
-            // '/dev/null' fallback driver, no cache at all
-            $handlers[] = 'BlackHole';
-        }
-
-        return array(
-            'tracking' => false,
-            'caches' => array(
-                'default' => array(
-                    'drivers' => $handlers,
-                    // inMemory will enable/disable "Ephemeral", not allowed as separate handler in stash-bundle
-                    'inMemory' => true,
-                    'registerDoctrineAdapter' => false
-                ) + $handlerSetting
             )
         );
     }
