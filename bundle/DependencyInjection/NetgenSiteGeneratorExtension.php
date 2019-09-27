383bb93cc4cdea114b378de8528c2607b204b5d2
Edi Modrić <edi.modric@gmail.com>
2019-09-27 15:33:41 +0200

    Rename yml files to have yaml extension

--- bundle/DependencyInjection/NetgenSiteGeneratorExtension.php
+++ bundle/DependencyInjection/NetgenSiteGeneratorExtension.php
@@ -14,6 +14,6 @@ final class NetgenSiteGeneratorExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
     }
 }
